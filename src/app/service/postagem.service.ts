4a5a25d4bc78e2a4ea62e1f48b877ab8cd6f36c0
chellebernardo <michellebernardo13@hotmail.com>
2021-07-07 18:33:43 -0300

    Implementa put de postagens

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -21,8 +21,20 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://backendblogpessoal.herokuapp.com/postagens', this.token)
   }
 
+  getByIdPostagem (id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://backendblogpessoal.herokuapp.com/postagens/${id}`)
+  }
+
   posPostagem (postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://backendblogpessoal.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  putPostagem (postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem> ('https://backendblogpessoal.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  deletePostagem (id: number){
+    this.http.delete(`https://backendblogpessoal.herokuapp.com/postagens/${id}`)
   }
 
 }

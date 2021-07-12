5e37e89647cd71750c489870f5e949b7caa73be1
chellebernardo <michellebernardo13@hotmail.com>
2021-07-12 09:59:57 -0300

    Implementação de put e delete de postagens

--- src/app/edit/postagem-edit/postagem-edit.component.ts
+++ src/app/edit/postagem-edit/postagem-edit.component.ts
@@ -28,6 +28,9 @@ export class PostagemEditComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
+    window.scroll (0,0)
+
     if (environment.token == ''){
       alert ('Sua seção expirou. Faça login novamente!')
       this.router.navigate(['/logar'])
